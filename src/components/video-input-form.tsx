1144ee0cd3a94248d695d6afb450253dc94631c3
Samuel Souza <smksouza.dev@gmail.com>
2023-09-22 13:57:43 -0300

    ulitização da biblioteca ffmpeg

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,11 +3,11 @@ import { Separator } from "@radix-ui/react-separator";
 import { FileVideo, Upload } from "lucide-react";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export const VideInputForm = () => {
   const [videoFile, setVideoFile] = useState<File | null>(null);
-
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget;
 
@@ -18,6 +18,16 @@ export const VideInputForm = () => {
     setVideoFile(selectedFile);
   }
 
+  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if(!videoFile) {
+      return
+    }
+  }
+
   const previewURL = useMemo(() => {
     if (!videoFile) {
       return null;
@@ -26,7 +36,7 @@ export const VideInputForm = () => {
   }, [videoFile]);
 
   return (
-    <form className="space-y-6">
+    <form onSubmit={handleUploadVideo} className="space-y-6">
       <label
         htmlFor="video"
         className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10"
@@ -57,6 +67,7 @@ export const VideInputForm = () => {
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
         <Textarea
+          ref={promptInputRef}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula(,)"
